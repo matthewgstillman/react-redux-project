d5b6bf99fbe3c9d336d08a20bc78b57995e53692
matthewgstillman <matthewgstillman@gmail.com>
2024-08-28 15:21:52 -0600

    latest updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import react from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Redux Project</h1>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? <h3>Valuable information I shouldn't see</h3> : ""}
     </div>
   );
 }
